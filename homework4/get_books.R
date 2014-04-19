1b5167f0cddf1a4eb8b42224730d502753595d8e
Jason Ament <jason.ament@gmail.com>
2014-04-18 22:25:01 -0700

    images

--- homework4/get_books.R
+++ homework4/get_books.R
@@ -1,6 +1,5 @@
 require(tm)
 require(SnowballC)
-require(ggplot2)
 
 book_source <- DirSource(
   directory = 'Documents/msan/classes/data_visualization/msan622/homework4/books/',
@@ -35,12 +34,6 @@ book_corpus <- tm_map(
   book_corpus, 
   stripWhitespace)
 
-# Remove specific words
-#sotu_corpus <- tm_map(
-#  sotu_corpus, 
-#  removeWords, 
-#  c("will", "can", "get", "that", "year", "let"))
-
 #print(book_corpus[['siddhartha.txt']])
 
 book_tdm <- TermDocumentMatrix(book_corpus)
@@ -48,7 +41,11 @@ book_tdm <- TermDocumentMatrix(book_corpus)
 #findFreqTerms(book_tdm, 20)
 
 book_matrix <- as.matrix(book_tdm)
+colnames(book_matrix) <- c("Beyond Good and Evil", "Siddhartha")
+#swapping order of the columns for the comparison cloud
+book_matrix <- book_matrix[,c(2,1)]
 
+#Create single frequency data frame
 freq_df <- data.frame(
   word = rownames(book_matrix), 
   # necessary to call rowSums if have more than 1 document
@@ -60,15 +57,39 @@ freq_df <- freq_df[with(
   freq_df, 
   order(freq, decreasing = TRUE)), ]
 
-# Do not need the row names anymore
 rownames(freq_df) <- NULL
 
-# Check out final data frame
-# View(sotu_df)
 
+
+#Find most frequent words for both texts
+sid_freq_df <- sort(book_matrix[,1], decreasing=TRUE)[1:15]
+sid_freq_df <- data.frame(
+  word = names(sid_freq_df),
+  freq = sid_freq_df)
+sid_freq_df <- sid_freq_df[order(sid_freq_df$freq),]
+
+sid_freq_df$word <- factor(sid_freq_df$word,
+                           levels = sid_freq_df$word,
+                           ordered = TRUE)
+rownames(sid_freq_df) <- NULL
+
+
+bge_freq_df <- sort(book_matrix[,2], decreasing=TRUE)[1:15]
+bge_freq_df <- data.frame(
+  word = names(bge_freq_df),
+  freq = bge_freq_df)
+bge_freq_df <- bge_freq_df[order(bge_freq_df$freq),]
+
+bge_freq_df$word <- factor(bge_freq_df$word,
+                           levels = bge_freq_df$word,
+                           ordered = TRUE)
+rownames(bge_freq_df) <- NULL
+
+
+#Create comparison data frame
 comp_df <- data.frame(
-  "Siddhartha" = book_matrix[, "siddhartha.txt"],
-  "Beyond Good and Evil" = book_matrix[, "beyondgande.txt"],
+  "Siddhartha" = book_matrix[,1],
+  "Beyond.Good.and.Evil" = book_matrix[,2],
   stringsAsFactors = FALSE)
 
 rownames(comp_df) <- rownames(book_matrix)
@@ -77,18 +98,10 @@ comp_df <- comp_df[order(
   rowSums(comp_df), 
   decreasing = TRUE),]
 
-comp_df_top <- head(comp_df, 15)
-
-# Plot frequencies
-p <- ggplot(comp_df_top, aes(Siddhartha,Beyond.Good.and.Evil))
-
-p <- p + geom_text(
-  label = rownames(comp_df_top),
-  position = position_jitter(
-    width = 2,
-    height = 2))
-print(p)
+comp_df_top <- head(comp_df, 25)
 
 
+#indexing by regex on rowname
+#book_matrix[grep('philosop', rownames(book_matrix)),]
 
 
---
+++ homework4/plots.R
@@ -0,0 +1,44 @@
+require(ggplot2)
+require(wordcloud)
+
+# Plot frequencies
+p <- ggplot(sid_freq_df, aes(x = word, y = freq)) +
+  geom_bar(stat = "identity", fill = "#018571") +
+  ggtitle("Top 15 Words by Frequency in Siddhartha") +
+  coord_flip() +
+  ylab("Frequency") +
+  theme_minimal() +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme(panel.grid = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  theme(axis.title.y = element_blank()) +
+  theme(axis.text.y  = element_text(face='bold', size=12))
+print(p)
+
+p <- ggplot(bge_freq_df, aes(x = word, y = freq)) +
+  geom_bar(stat = "identity", fill = "#a6611a") +
+  ggtitle("Top 15 Words by Frequency in Beyond Good and Evil") +
+  coord_flip() +
+  ylab("Frequency") +
+  theme_minimal() +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme(panel.grid = element_blank()) +
+  theme(axis.ticks = element_blank()) + 
+  theme(axis.title.y = element_blank()) +
+  theme(axis.text.y  = element_text(face='bold', size=12))
+
+print(p)
+
+png(file="SidvsBGE.png",height=800,width=1200)
+par(mfrow=c(1,2))
+
+comparison.cloud(book_matrix,
+  max.words=150,
+  random.order=FALSE,
+  colors=c('#018571', '#a6611a'),
+  title.size=1)
+
+commonality.cloud(book_matrix,random.order=FALSE, max.words=100, colors='blue')
+dev.off()
