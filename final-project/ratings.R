a10fc086a6b43e6e400ef51ecdd0d06c1ffd2b62
Jason Ament <jason.ament@gmail.com>
2014-05-15 16:49:39 -0700

    updated file location

--- final-project/ratings.R
+++ final-project/ratings.R
@@ -1,6 +1,6 @@
 
 
-ratings <- read.csv('./ratings.dat', sep='\t')
+ratings <- read.csv('ratings.dat', sep='\t')
 
 colnames(ratings) <- c('user_id', 'movie_id', 'rating', 'timestamp')
 #ratings$timestamp <- as.POSIXct('1970-01-01') + ratings$timestamp
